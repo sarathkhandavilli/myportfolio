fb08411801d0c5857ffefc5b1dbf35c15f699391
Sarath Chandra <132736634+sarathkhandavilli@users.noreply.github.com>
2025-05-19 20:11:34 +0530

    Update Work.jsx

--- components/Work.jsx
+++ components/Work.jsx
@@ -10,7 +10,7 @@ const Work = () => {
 
         <div className='grid grid-cols-auto my-10 gap-5'>
             {workData.map((project, index) => (
-                <div key={index} className='rounded-lg px-4 py-6 bg-white shadow-lg shadow-gray-300 cursor-pointer border-t border-gray-100 hover:shadow-gray-500 duration-500'>
+                <div key={index} className='rounded-lg px-4 py-6 bg-customBlue shadow-lg shadow-gray-300 cursor-pointer border-t border-gray-100 hover:shadow-gray-500 duration-500'>
                     <h3 className='text-lg my-4 text-gray-900 font-Outfit'>{project.title}</h3>
                     <p className='text-lg my-4 text-gray-600 leading-5 font-Outfit'>
                         {project.description}
